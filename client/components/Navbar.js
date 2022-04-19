4b122f97ebe796a18a077abeed35bd99d85499b1
Zach Sonnenblick <zach.sonnenblick@gmail.com>
2022-04-19 19:28:57 -0400

    added store to navbar and little style

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -4,8 +4,8 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>FS-App-Template</h1>
+  <div className='nav-bar'>
+    <h1>Grace Shopper Honeydew</h1>
     <nav>
       {isLoggedIn ? (
         <div>
@@ -20,6 +20,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
+          <Link to="/cars">Store</Link>
         </div>
       )}
     </nav>
