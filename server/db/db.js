d0be5806b3fd674389bafe238ebdaa86323ad41f
ChrisNieves <nievescs20@gmail.com>
2022-04-25 19:47:50 -0400

    teting out some db connection code

--- server/db/db.js
+++ server/db/db.js
@@ -4,11 +4,28 @@ const pkg = require("../../package.json");
 
 console.log(chalk.yellow("Opening database connection"));
 
+const databaseName =
+  pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
+
+const config = {
+  logging: false,
+};
+
+if (process.env.LOGGING === "true") {
+  delete config.logging;
+}
+
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
+
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`,
-  {
-    logging: false,
-  }
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  config
 );
 
 module.exports = db;
