103a2d2d0adb185b46fc8b52fea714111e0204e9
ChrisNieves <nievescs20@gmail.com>
2022-04-25 19:58:13 -0400

    undo

--- server/db/db.js
+++ server/db/db.js
@@ -4,24 +4,21 @@ const pkg = require("../../package.json");
 
 console.log(chalk.yellow("Opening database connection"));
 
-// const databaseName =
-//   pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
-
 const config = {
   logging: false,
 };
 
-if (process.env.LOGGING === "true") {
-  delete config.logging;
-}
+// if (process.env.LOGGING === "true") {
+//   delete config.logging;
+// }
 
-if (process.env.DATABASE_URL) {
-  config.dialectOptions = {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  };
-}
+// if (process.env.DATABASE_URL) {
+//   config.dialectOptions = {
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   };
+// }
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`,
