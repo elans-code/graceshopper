68e835de1cd6ca37231963d276c7ddce76a2aef2
ChrisNieves <nievescs20@gmail.com>
2022-04-26 15:12:44 -0400

    heroku

--- server/db/db.js
+++ server/db/db.js
@@ -8,17 +8,17 @@ const config = {
   logging: false,
 };
 
-// if (process.env.LOGGING === "true") {
-//   delete config.logging;
-// }
+if (process.env.LOGGING === "true") {
+  delete config.logging;
+}
 
-// if (process.env.DATABASE_URL) {
-//   config.dialectOptions = {
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//   };
-// }
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`,
